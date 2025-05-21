ce21772ea7b862df0cc7f09b58b3b1e39e662d38
JuanAntonio068 <ja.montano@iesnervion.es>
2025-05-21 10:16:07 +0200

    Avanzando

--- TrabajoCRUD/src/dao/CalificacionDAO.java
+++ TrabajoCRUD/src/dao/CalificacionDAO.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 import entidades.Calificacion;
 
@@ -27,7 +28,11 @@ public class CalificacionDAO {
 			System.out.println("Error al crear la conexión con la base de datos: " + e.getMessage());
 		}
 	}
-
+	
+	public Connection getConexion() {
+		return conexion;
+	}
+	
 	public void create(Calificacion calf) {
 		String sql = "INSERT INTO CALIFICACIONES (id_estudiante, id_curso, id_profesor, tipo_evaluacion, nota, fecha_evaluacion) VALUES (?, ?, ?, ?, ?, ?";
 
@@ -40,11 +45,32 @@ public class CalificacionDAO {
 			ps.setInt(3, calf.getProfesorId());
 			ps.setString(4, calf.getTipoEvaluacion());
 			ps.setDouble(5, calf.getNota());
-			ps.setString(7, calf.getFechaEvaluacion());
+			ps.setString(6, calf.getFechaEvaluacion());
 
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println("Error al insertar la calificación: " + e.getMessage());
 		}
+	}
+
+	public List<Calificacion> listarPorEstudiante(int estudianteId) {
+		return null;
+			
+			
+	}
+
+	public List<Calificacion> listarPorCurso(int cursoId){
+		return null;
+		
+	}
+	
+	public boolean update(int id, double nota) {
+		return false;
+		
+	}
+	
+	public boolean delete(int id) {
+		return false;
+		
 	}
 }
--- TrabajoCRUD/src/interfaz/Main.java
+++ TrabajoCRUD/src/interfaz/Main.java
@@ -2,35 +2,243 @@ package interfaz;
 
 import java.util.Scanner;
 
+import dao.CalificacionDAO;
 import entidades.Calificacion;
-import entidades.Curso;
-import entidades.Estudiante;
-import entidades.Profesor;
 
 public class Main {
 
+	// Declaramaos el escaner
 	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
+		// Declaramos el objeto
+		Calificacion calf;
+
+		// Declaramos el DAO
+		CalificacionDAO calfDAO = new CalificacionDAO();
+
+		// Variable para guardar la opción del usuario
+		int opcion;
+
+		// Variable para guardar el id de una calificacion
+		int id;
+
+		// Variable para guardar el id de un estudiante
+		int estudianteId;
+
+		// Variable para guardar el id de un curso
+		int cursoId;
+
+		// Variable para guardar la nota
+		double nota;
+
+		// Valida si la conexion ha sido correcta
+		if (calfDAO.getConexion() != null) {
+			// Bucle que se repite hasta que el usuario decide salir
+			do {
+				// Llamamos a la función para imprimir el menú
+				menu();
+				// Leeemos la opción que ha elegido el usuario
+				opcion = sc.nextInt();
+				// Limpiamos el escaner
+				sc.nextLine();
+
+				// Switch para marcar las diferentes opciones
+				switch (opcion) {
+				// El usuario ha elegido agregar una calificacion
+				case 1 -> {
+					// Llamamos a la funcion para pedir los datos y crear el objeto
+					calf = leerDatosCalificacion();
+
+					// Llamamos a la función para crear la calificación en la base de datos
+					calfDAO.create(calf);
+				}
+				// El usuario ha elegido listar todas las calificaciones de un estudiante
+				case 2 -> {
+
+					// Llamamos al método y lo guardamos
+					estudianteId = pedirEstudianteId();
+
+					// Llamamos al método para listar todas las calificaciones
+					calfDAO.listarPorEstudiante(estudianteId);
+				}
+				// El usuario ha elegido listar todas las calificaciones de un curso
+				case 3 -> {
+					// Llamamos al método y lo guardamos
+					cursoId = pedirCursoId();
+
+					// Llamamos al método para listar todas las calificaciones
+					calfDAO.listarPorCurso(cursoId);
+				}
+				// El usuario ha elegido modificar una calificación
+				case 4 -> {
+					// Llamamos al método y lo guardamos
+					id = pedirId();
+
+					// Llamamos al método y la guardamos
+					nota = pedirNota();
+
+					// Llamamos al método para modificar una calificación
+					calfDAO.update(id, nota);
+				}
+				// El usuario ha elegido eliminar una calificación
+				case 5 -> {
+					// Llamamos al método y lo guardamos
+					id = pedirId();
+
+					// Llamamos al método para eliminar una calificación
+					calfDAO.delete(id);
+				}
+				// El usuario ha elegido salir del bucle
+				case 0 -> {
+					System.out.println("Saliendo...");
+				}
+				// El usuario no ha elegido una opción incorrecta
+				default -> {
+					System.out.println("Opción incorrecta \nElige una opción válida");
+				}
+				}
+			} while (opcion != 0);
+
+			// Cerramos el escaner
+			sc.close();
+		}
 	}
 
 	// Método para mostar el menú
-	public void menu() {
+	public static void menu() {
 		System.out.println("1. Crear calificación");
 		System.out.println("2. Listar todas las calificaciones de un estudiante");
 		System.out.println("3. Listar todas las calificaciones de un curso");
 		System.out.println("4. Modificar una calificación");
 		System.out.println("5. Eliminar calificación");
+		System.out.println("0. Salir");
 	}
 
 	// Método para leer los datos
-	public Calificacion leerDatos() {
-		Calificacion c1 = null; 
-		Estudiante e1 = null;
-		Curso cu1 = null;
-		Profesor p1 = null;
-		
-		
+	public static Calificacion leerDatosCalificacion() {
+		// Declaramos el objeto
+		Calificacion c1 = null;
+
+		// Variable para guardar el id del estudiante
+		int idEstudiante;
+
+		// Variable para guardar el id del curso
+		int idCurso;
+
+		// Variable para guardar el id del profesor
+		int idProfesor;
+
+		// Variable para guardar el tipo de evaluación
+		String tipoEvaluacion;
+
+		// Variable para guardar la nota
+		double nota;
+
+		// Variable para guardar la fecha de evaluación
+		String fechaEvaluacion;
+
+		// Ejecutamos la función para pedir el id del estudiante y lo guardamos
+		idEstudiante = pedirEstudianteId();
+
+		// Ejecutamos la función para pedir el id del curso y lo guardamos
+		idCurso = pedirCursoId();
+
+		// Ejecutamos la función para pedir el id del profesor y lo guardamos
+		idProfesor = pedirProfesorId();
+
+		// Preguntamos por el tipo de evaluación y lo guardamos
+		System.out.println("Introduce el tipo de evaluación");
+		tipoEvaluacion = sc.nextLine();
+
+		// Ejecutamos la función para pedir la nota y la guardamos
+		nota = pedirNota();
+
+		// Preguntamos por la fecha de evaluación y lo guardamos
+		System.out.println("Introduce la fecha de evaluación");
+		fechaEvaluacion = sc.nextLine();
+
+		// Creamos el objeto con todos los datos
+		c1 = new Calificacion(idEstudiante, idCurso, idProfesor, tipoEvaluacion, nota, fechaEvaluacion);
+
+		// Devolvemos el objeto
+		return c1;
+	}
+
+	// Método para pedir el id de la calificación
+	public static int pedirId() {
+		// Variable para guardar el id de la calificación
+		int id;
+
+		// Preguntamos el id de la calificación y lo guardamos
+		System.out.println("Introduce el id de la calificación");
+		id = sc.nextInt();
+
+		// Limpiamos el escaner
+		sc.nextLine();
+
+		// Devolvemos el id de la calificación
+		return id;
+	}
+
+	// Método para pedir el id del estudiante
+	public static int pedirEstudianteId() {
+		// Variable para guardar la id del estudiante
+		int idEstudiante;
+
+		// Preguntamos por el id del estudiante y lo guardamos
+		System.out.println("Introduce el id del estudiante");
+		idEstudiante = sc.nextInt();
+		// Limpiamos el escaner
+		sc.nextLine();
+
+		// Devolvemos el id del estudiante
+		return idEstudiante;
+	}
+
+	// Método para pedir el id del curso
+	public static int pedirCursoId() {
+		// Variable para guardar la id del curso
+		int idCurso;
+
+		// Preguntamos por el id del curso y lo guardamos
+		System.out.println("Introduce el id del curso");
+		idCurso = sc.nextInt();
+		// Limpiamos el escaner
+		sc.nextLine();
+
+		// Devolvemos el id del curso
+		return idCurso;
+	}
+
+	// Método para pedir el id del profesor
+	public static int pedirProfesorId() {
+		// Variable para guardar el id del profesor
+		int idProfesor;
+
+		// Preguntamos por el id del profesor y lo guardamos
+		System.out.println("Introduce el id del profesor");
+		idProfesor = sc.nextInt();
+		// Limpiamos el escaner
+		sc.nextLine();
+
+		// Devolvemos el id del profesor
+		return idProfesor;
+	}
+
+	// Método para pedir la nota
+	public static double pedirNota() {
+		// Variable para guardar la nota
+		double nota;
+
+		// Preguntamos por la nota y la guardamos
+		System.out.println("Introduce la nota");
+		nota = sc.nextDouble();
+		// Limpiamos el escaner
+		sc.nextLine();
+
+		// Devolvemos la nota
+		return nota;
 	}
 }
